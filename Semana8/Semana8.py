c9aa252ac2cc52e2b20bd4e69022f886ce3439aa
teb92 <147997027+teb92@users.noreply.github.com>
2025-07-29 12:52:39 -0600

    Update Semana8.py

--- Semana8/Semana8.py
+++ Semana8/Semana8.py
@@ -3,8 +3,8 @@ from cache import CacheManager
 
 
 redis_client = CacheManager(
-    host="redis-19655.c52.us-east-1-4.ec2.redns.redis-cloud.com",
-    port=19655,
-    password="A1tNBns2jEpGPrkHIBpzjTvm2UoLM6bx",
+    host="rXXX",
+    port=XX,
+    password="A1XXXX",
 )
 
